c1d2db7fe8ea70209ad01f4eb18aee919fa59b43
AlexMel83 <meleshko.a.n@ukr.net>
2025-02-08 19:59:34 +0200

    clean project

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
 #include <time.h>
-#include "alert_logic.h"
 
 #define DEBUG
 
-AlertLogic alertLogic;
-
 void setup() {
-	Serial.begin(115200);
+	
 
-	alertLogic = AlertLogic();
-	alertLogic.Init();
 }
 
+
 void loop() {
-	alertLogic.Tick(); 
+
 }
